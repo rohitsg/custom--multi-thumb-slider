8d79540c11129ca1eb2da16aa9bb77295104e2ac
rohit goudagaon <rohitsgoudagaon@gmail.com>
2023-07-19 08:58:46 +0530

    added another custom slider with shape triangle

--- vanila slider/index.js
+++ vanila slider/index.js
@@ -3,21 +3,32 @@ function run() {
   let thumbDragging = false;
 
   const lowEle = document.querySelector('.low')
+  const midEle = document.querySelector('.mid')
   const sliderContainerEle = document.querySelector('.slider-container')
   lowEle.addEventListener('mousedown', function(e) {
-    console.log('low down- ', e.target)
+    console.log('low down- ')
     mouseDown = true;
     e.stopPropagation();
   });
-
-  lowEle.addEventListener('mousemove', function(e) {
+  
+  sliderContainerEle.addEventListener('mousemove', function(e) {
     e.stopPropagation();
-    console.log('low move- ', e.target)
     mousemove = true;
+  const midEle = document.querySelector('.mid')
     
     if (mouseDown) {
       thumbDragging = true;
-      console.log('Hndling drag...')
+      // lowEle.style.position = 'absolute';
+      let lowEleLeft = lowEle.getBoundingClientRect().left;
+      let midEleLeft = midEle.getBoundingClientRect().left - lowEle.getBoundingClientRect().width - 5;
+      let diff = midEleLeft - lowEleLeft;
+      let shiftX = e.clientX - lowEle.getBoundingClientRect().left;
+      console.log('sliderContainerEle move- ',shiftX, e.pageX, e.pageX - shiftX)
+      if (lowEleLeft < midEleLeft) {
+        // if (diff > e.offsetX) {
+          // lowEle.style.left =  e.offsetX + 'px';
+        }
+        lowEle.style.left =  e.pageX - shiftX + 'px';
     }
   });
 
@@ -31,10 +42,15 @@ function run() {
     e.stopPropagation();
   });
 
-  sliderContainerEle.addEventListener('mousedown', function(e) {
-    console.log('container - ', e.target)
-    mouseDown = true;
+  sliderContainerEle.addEventListener('mouseup', function(e) {
+    console.log('sliderContainerEle up - ', e.target) 
+    mouseDown = false;
+    mousemove = false;
+    thumbDragging = false;
+
+    e.stopPropagation();
   });
+
 }
 
 run();
